e5343a18f48c424f44bb71e101cf36d3f3a1d4d2
Enrique <enriquedlc2d@gmail.com>
2023-06-08 12:27:20 +0100

    :construction:: working in the header component.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/react-in-jsx-scope */
+
+const Header = () => {
+	return (
+		<header>
+			<div className="container header-container">
+				<h5>Hello I'm</h5>
+				<h1>Enrique De La Concepción</h1>
+				<h2 className="light-text" >FullStack Developer</h2>
+			</div>
+		</header>
+	)
+}
+
+export default Header
